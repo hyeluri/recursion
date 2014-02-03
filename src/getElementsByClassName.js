50a29f6fc36727172818028a3d51dba9d22b377d
hyeluri <hyeluri@gmail.com>
2014-02-03 15:43:51 +0530

    finished getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,5 +4,35 @@
  //};
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
-  // your code here
+
+var results=[];
+
+var mainBod = document.body;
+
+findInChild(mainBod);
+
+//function to walk through the DOM tree starting at childElement
+	function findInChild(childElement){
+
+		if(childElement.hasChildNodes()){
+			var children = childElement.firstChild;
+			//had to use while loop beacuse could not use .length on some node types
+			while(children){
+				//check to see if node is an HTMLElement
+				if(children.nodeType === 1){
+					if(children.classList.contains(className)){
+						results.push(children);
+					}
+					//recursively walk through the DOM tree(interested in only HTMLElement nodes)
+					findInChild(children);
+				}
+				//advance to next node of this branch
+				children = children.nextSibling;
+			}
+		};
+	};
+
+return results;
+
 };
+
